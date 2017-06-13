0cc3a3bb23667c7e6103e3883351b0c03eb50a22
Murthy Kakarlamudi <murthy@modusbox.com>
2017-06-13 11:29:13 -0400

    Added junit support using wiremock

--- src/test/java/com/l1p/interop/scheme/adapter/TestSchemeAdapter.java
+++ src/test/java/com/l1p/interop/scheme/adapter/TestSchemeAdapter.java
@@ -1,40 +1,23 @@
 package com.l1p.interop.scheme.adapter;
 
-import java.io.IOException;
-import java.security.KeyManagementException;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.security.cert.CertificateException;
-
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 import org.mule.tck.junit4.FunctionalTestCase;
 
-import com.sun.jersey.api.client.Client;
-import com.sun.jersey.api.client.ClientResponse;
-import com.sun.jersey.api.client.WebResource;
-import com.sun.jersey.api.client.config.ClientConfig;
-import com.sun.jersey.api.client.config.DefaultClientConfig;
 
-//import com.github.tomakehurst.wiremock.junit.WireMockRule;
-//import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
 
-//import io.restassured.RestAssured;
-//import io.restassured.response.Response;
-//import static io.restassured.RestAssured.given;
-
-//import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import io.restassured.response.Response;
+import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
 
 public class TestSchemeAdapter extends FunctionalTestCase {
 	
-	//@Rule
-    //public WireMockRule wireMockRule = new WireMockRule(8010);
+	@Rule
+    public WireMockRule wireMockRule = new WireMockRule(8010);
 	
-	private final String serviceHost = "http://localhost:8088";
-	
-	WebResource webService;
 	
 	@Override
 	protected String getConfigResources() {
@@ -47,32 +30,24 @@ public class TestSchemeAdapter extends FunctionalTestCase {
 		System.setProperty("spring.profiles.active", "test");
 	}
 	
-	@Before
-	public void initSslClient() throws NoSuchAlgorithmException, KeyStoreException, CertificateException, IOException,
-			KeyManagementException {
-		ClientConfig config = new DefaultClientConfig();
-		webService = Client.create(config).resource(serviceHost);
-	}
 	
 	@Test
-	public void exampleTest() throws Exception {
-		String proxyQuoteRequestJson = loadResourceAsString("test_data/proxyQuoteRequest.json");
+	public void testQuotes() throws Exception {
 		String dfspQuoteResponseJson = loadResourceAsString("test_data/dfspQuoteResponse.json");
-		//givenThat(post(urlMatching("/v1/quote")).willReturn(aResponse().withBody(dfspQuoteResponseJson)));
+		givenThat(post(urlMatching("/v1/quote")).willReturn(aResponse().withBody(dfspQuoteResponseJson)));
 
-	    //Thread.sleep(15000);
-//	    Response response =
-//        	given().
-//            	contentType("application/json").
-//            	body(proxyQuoteRequestJson).
-//            when().
-//            	post("http://localhost:8088/scheme/adapter/v1/quotes");
+		String proxyQuoteRequestJson = loadResourceAsString("test_data/proxyQuoteRequest.json");
+		Response response =
+        	given().
+            	contentType("application/json").
+            	body(proxyQuoteRequestJson).
+            when().
+            	post("http://localhost:8088/scheme/adapter/v1/quotes");
 
-		ClientResponse clientResponse = postRequest( "/scheme/adapter/v1/quotes", proxyQuoteRequestJson );
+	    assertEquals("payeeFee",(String)response.jsonPath().get("payeeFee.amount"),"1");
+	    assertEquals("payeeCommission",(String)response.jsonPath().get("payeeCommission.amount"),"1");
+		
 	}
 	
-	private ClientResponse postRequest( String path, String requestData ) {
-		return webService.path( path ).type( "application/json").post(ClientResponse.class, requestData);
-	}
 
 }
