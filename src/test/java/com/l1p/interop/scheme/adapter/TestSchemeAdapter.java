253aaaea601f3b87f48dda302ea9eb5bd5858102
Murthy Kakarlamudi <murthy@modusbox.com>
2017-07-21 12:21:33 -0400

    Updates for notifications endpoint

--- src/test/java/com/l1p/interop/scheme/adapter/TestSchemeAdapter.java
+++ src/test/java/com/l1p/interop/scheme/adapter/TestSchemeAdapter.java
@@ -91,7 +91,7 @@ public class TestSchemeAdapter extends FunctionalTestCase {
 		assertEquals("payeeFee","1",(String)response.jsonPath().get("payeeFee.amount"));
 	    assertEquals("payeeCommission","1",(String)response.jsonPath().get("payeeCommission.amount"));
 	    assertEquals("ipr","Aojf9Pq9_RKgnS3mzvYnZAXvJuvjWnw6r-JXdwitLmHygdQBgdEAAAAAAAAEsDZsZXZlbG9uZS5kZnNwMS5hbGljZS5TdXVPNUdhaDUxSXM3VzVyUkdXdVBnTWVSdGtKOXZPelGBj1BTSy8xLjAKTm9uY2U6IHRsNF93NVRfaGhLM0FFcWJ3Ukg3VVEKRW5jcnlwdGlvbjogbm9uZQpQYXltZW50LUlkOiAxMTBlYzU4YS1hMGYyLTRhYzQtODM5My1jODY2ZDgxM2I4ZDEKCkV4cGlyZXMtQXQ6IDIwMTctMDYtMjBUMDA6MDA6MDEuMDAwWgoKAA==",(String)response.jsonPath().get("ipr"));
-		assertEquals("connectorAccount","http://ec2-52-32-130-4.us-west-2.compute.amazonaws.com:8088/ilp/ledger/v1/accounts/dfsp1-testconnector",(String)response.jsonPath().get("connectorAccount"));
+		assertEquals("connectorAccount","http://ec2-35-166-189-14.us-west-2.compute.amazonaws.com:8014/ledger/accounts/dfsp1-testconnector",(String)response.jsonPath().get("connectorAccount"));
 	    assertEquals("expiresAt","2017-06-14T00:00:01.000Z",(String)response.jsonPath().get("expiresAt"));
 	}
 	
